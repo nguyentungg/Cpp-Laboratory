d0fe1a68d8ae330aaac039cee50b3e46485e7a41
Nguyen Tung <thanhtung4693@gmail.com>
2023-03-25 23:33:35 +0700

    Add notification about how C++ function will work

--- CppLaboratory/Source.cpp
+++ CppLaboratory/Source.cpp
@@ -1,6 +1,8 @@
 #include<iostream>;
 using namespace std;
 
+// This is call function prototype, Because main() function call function before the function itself was define. 
+// So in C++ we can not call the function before define the function before main().
 void welcome();
 char getYesNo();
 void printResponse(char responseToPrint);
