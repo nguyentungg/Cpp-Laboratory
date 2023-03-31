4586c33c8f6bf3a78b479ef78ab7a0352d88238f
Nguyen Tung <thanhtung4693@gmail.com>
2023-04-01 04:47:35 +0700

    Destructors

---
+++ CppLaboratory/Destructors.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Character
+{
+public:
+
+	Character();
+	~Character();
+
+	int* CharacterAge;
+	float* CharacterHealth;
+};
+
+int main() {
+
+	Character* Char = new Character;
+	delete Char;
+
+	system("pause"); 
+}
+
+Character::Character() {
+	cout << "A new character was created .\n";
+
+	CharacterAge = new int(1);
+	CharacterHealth = new float(100.f);
+}
+
+Character::~Character() {
+	cout << "Character destroyed.\n";
+
+	delete CharacterAge;
+	delete CharacterHealth;
+}
--- CppLaboratory/StackAndHeap.cpp
+++ CppLaboratory/StackAndHeap.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-struct Character 
+struct CharacterSH
 {
 
-	Character();
+	CharacterSH();
 
 	void PrintHealth();
 
@@ -13,30 +13,30 @@ struct Character
 	float Health;
 };
 
-Character::Character() {
+CharacterSH::CharacterSH() {
 	Name = "Default Name";
 	Health = 5.f;
 }
 
-void Character::PrintHealth() {
+void CharacterSH::PrintHealth() {
 	cout << "Health = " << Health << endl;
 }
 
-int main(){
-	
-
-	for (int i = 0; i < 10; i++) {
-		Character* PtrToChar = new Character();
-
-		cout << PtrToChar->Name << endl;
-
-		PtrToChar->PrintHealth();
-		
-		delete PtrToChar;
-	}
-
-
-
-
-	system("pause"); 
-}
+//int main(){
+//	
+//
+//	for (int i = 0; i < 10; i++) {
+//		CharacterSH* PtrToChar = new CharacterSH();
+//
+//		cout << PtrToChar->Name << endl;
+//
+//		PtrToChar->PrintHealth();
+//		
+//		delete PtrToChar;
+//	}
+//
+//
+//
+//
+//	system("pause"); 
+//}
