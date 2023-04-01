08ee8c083dcb2124357f7964f3504604ee607aa6
Nguyen Tung <thanhtung4693@gmail.com>
2023-04-01 17:02:15 +0700

    Static

--- CppLaboratory/Destructors.cpp
+++ CppLaboratory/Destructors.cpp
@@ -13,24 +13,24 @@ public:
 	float* CharacterHealth;
 };
 
-int main() {
-
-	Character* Char = new Character;
-	delete Char;
-
-	system("pause"); 
-}
-
-Character::Character() {
-	cout << "A new character was created .\n";
-
-	CharacterAge = new int(1);
-	CharacterHealth = new float(100.f);
-}
-
-Character::~Character() {
-	cout << "Character destroyed.\n";
-
-	delete CharacterAge;
-	delete CharacterHealth;
-}
+//int main() {
+//
+//	Character* Char = new Character;
+//	delete Char;
+//
+//	system("pause"); 
+//}
+//
+//Character::Character() {
+//	cout << "A new character was created .\n";
+//
+//	CharacterAge = new int(1);
+//	CharacterHealth = new float(100.f);
+//}
+//
+//Character::~Character() {
+//	cout << "Character destroyed.\n";
+//
+//	delete CharacterAge;
+//	delete CharacterHealth;
+//}
---
+++ CppLaboratory/StaticKeyword.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+void AddToCount() {
+	static int count = 0;
+	count++;
+	cout << count << endl;
+}
+
+class Item
+{
+public:
+	Item();
+	~Item();
+
+private:
+
+};
+
+Item::Item()
+{
+	cout << "An item has been created!\n";
+}
+
+Item::~Item()
+{
+	cout << "An item has been destroyed!\n";
+}
+
+class Critter {
+public:
+
+	Critter() {
+		cout << "A critter is born\n";
+		++CritterCount;
+	}
+	static void AnnounceCount() {
+		cout << CritterCount << endl;
+	}
+
+	static int CritterCount;
+
+};
+
+int Critter::CritterCount = 0;
+
+int main() {
+	/*AddToCount();
+	{ Item item; }*/
+
+	//Critter::CritterCount = 13;
+
+	//Critter crit;
+	//cout << Critter::CritterCount << endl;
+
+	//Critter crit2;
+	//cout << Critter::CritterCount << endl;
+
+	Critter::AnnounceCount();
+
+	Critter* crit = new Critter;
+	Critter::AnnounceCount();
+	delete crit;
+
+	system("pause"); 
+}
