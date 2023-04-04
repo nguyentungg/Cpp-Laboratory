d9426e88e3fcbea686ec4042c0af4e1b5491cf70
Nguyen Tung <thanhtung4693@gmail.com>
2023-04-04 15:35:05 +0700

    Casting And Type Conversion

--- CppLaboratory/Polymorphism.cpp
+++ CppLaboratory/Polymorphism.cpp
@@ -1,49 +1,49 @@
 #include <iostream>
 
 using namespace std;
-class Object {
+class ObjectP {
 public:
 	virtual void BeginPlay();
 };
 
-class Actor : public Object
+class ActorP : public ObjectP
 {
 public:
 	virtual void BeginPlay() override;
 };
-class Pawn : public Actor
+class PawnP : public ActorP
 {
 public:
 	virtual void BeginPlay() override;
 };
-int main() {
+//int main() {
+//
+//
+//	ObjectP* ptr_to_object = new ObjectP;
+//	ActorP* ptr_to_actor = new ActorP;
+//	PawnP* ptr_to_pawn = new PawnP;
+//
+//	ObjectP* ObjectPArray[] = { ptr_to_object, ptr_to_actor, ptr_to_pawn };
+//
+//	for (int i = 0; i < 3; i++) {
+//		ObjectPArray[i]->BeginPlay();
+//	}
+//
+//	delete ptr_to_object;
+//	delete ptr_to_actor;
+//	delete ptr_to_pawn;
+//
+//	system("pause");
+//}
 
-
-	Object* ptr_to_object = new Object;
-	Actor* ptr_to_actor = new Actor;
-	Pawn* ptr_to_pawn = new Pawn;
-
-	Object* ObjectArray[] = { ptr_to_object, ptr_to_actor, ptr_to_pawn };
-
-	for (int i = 0; i < 3; i++) {
-		ObjectArray[i]->BeginPlay();
-	}
-
-	delete ptr_to_object;
-	delete ptr_to_actor;
-	delete ptr_to_pawn;
-
-	system("pause");
+void ObjectP::BeginPlay() {
+	cout << "ObjectP BeginPlay() called. \n";
 }
 
-void Object::BeginPlay() {
-	cout << "Object BeginPlay() called. \n";
+void ActorP::BeginPlay() {
+	cout << "ActorP BeginPlay() called. \n";
 }
 
-void Actor::BeginPlay() {
-	cout << "Actor BeginPlay() called. \n";
-}
-
-void Pawn::BeginPlay() {
-	cout << "Pawn BeginPlay() called. \n";
+void PawnP::BeginPlay() {
+	cout << "PawnP BeginPlay() called. \n";
 }
